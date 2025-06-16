76e05c3674be97febc1a90e453085fe66aab8081
emanuelekm <emanuelekmiecik1507@gmail.com>
2025-06-16 09:23:18 -0300

    UC_Relatorio.cs

--- GerenciamentoBiblioteca/UC_Relatorios.Designer.cs
+++ GerenciamentoBiblioteca/UC_Relatorios.Designer.cs
@@ -28,7 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
+            this.chartEmprestimo = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridViewEmprestimo = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chartEmprestimo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmprestimo)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -42,13 +49,46 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Relatórios";
             // 
+            // chartEmprestimo
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartEmprestimo.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartEmprestimo.Legends.Add(legend1);
+            this.chartEmprestimo.Location = new System.Drawing.Point(46, 90);
+            this.chartEmprestimo.Name = "chartEmprestimo";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartEmprestimo.Series.Add(series1);
+            this.chartEmprestimo.Size = new System.Drawing.Size(747, 448);
+            this.chartEmprestimo.TabIndex = 1;
+            this.chartEmprestimo.Text = "chartEmprestimo";
+            // 
+            // dataGridViewEmprestimo
+            // 
+            this.dataGridViewEmprestimo.AllowUserToAddRows = false;
+            this.dataGridViewEmprestimo.AllowUserToDeleteRows = false;
+            this.dataGridViewEmprestimo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmprestimo.Location = new System.Drawing.Point(24, 562);
+            this.dataGridViewEmprestimo.Name = "dataGridViewEmprestimo";
+            this.dataGridViewEmprestimo.ReadOnly = true;
+            this.dataGridViewEmprestimo.RowHeadersWidth = 62;
+            this.dataGridViewEmprestimo.RowTemplate.Height = 28;
+            this.dataGridViewEmprestimo.Size = new System.Drawing.Size(1313, 280);
+            this.dataGridViewEmprestimo.TabIndex = 2;
+            // 
             // UC_Relatorios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridViewEmprestimo);
+            this.Controls.Add(this.chartEmprestimo);
             this.Controls.Add(this.label1);
             this.Name = "UC_Relatorios";
-            this.Size = new System.Drawing.Size(1128, 550);
+            this.Size = new System.Drawing.Size(1378, 874);
+            ((System.ComponentModel.ISupportInitialize)(this.chartEmprestimo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmprestimo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +97,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartEmprestimo;
+        private System.Windows.Forms.DataGridView dataGridViewEmprestimo;
     }
 }
--- GerenciamentoBiblioteca/UC_Relatorios.cs
+++ GerenciamentoBiblioteca/UC_Relatorios.cs
@@ -7,14 +7,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
 
 namespace GerenciamentoBiblioteca
 {
     public partial class UC_Relatorios : UserControl
     {
+        private MySqlConnection conexao;
+        private Timer timerAtualizacao;
+        private DataGridView dataGridViewRelatorio;
+
         public UC_Relatorios()
         {
             InitializeComponent();
+
+            string conexaoString = "Server=localhost;Database=gerenciamentobiblioteca;Uid=root;Pwd=;";
+            conexao = new MySqlConnection(conexaoString);
+
+            //InicializarDataGridView();
+            InicializarTimer();
+            CarregarEmprestimosNoDataGridView();
+        }
+        /*private void InicializarDataGridView()
+        {
+            dataGridViewRelatorio = new DataGridView();
+            {
+                Dock = DockStyle.Fill,
+                ReadOnly = true,
+                AutoGenerateColumns = true,
+                AllowUserToAddRows = false,
+            };
+            this.Controls.Add(dataGridViewRelatorio);
+        }*/
+
+        private void InicializarTimer()
+        {
+            timerAtualizacao = new Timer
+            {
+                Interval = 5000 // 5 segundos
+            };
+            timerAtualizacao.Tick += (s, e) => CarregarEmprestimosNoDataGridView();
+            timerAtualizacao.Start();
+        }
+        private void CarregarEmprestimosNoDataGridView()
+        {
+            try
+            {
+                var lista = ObterTodosEmprestimos();
+                // Supondo que você já tenha adicionado pelo designer
+                // um DataGridView chamado dataGridViewEmprestimo
+                dataGridViewEmprestimo.DataSource = null;
+                dataGridViewEmprestimo.DataSource = lista;
+                AtualizarGrafico(lista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar empréstimos: " + ex.Message);
+            }
+        }
+
+        private void AtualizarGrafico(List<Emprestimo> emprestimos)
+        {
+            // Supondo que você já tenha adicionado pelo designer
+            // um Chart chamado chartEmprestimos
+            var chart = chartEmprestimo;
+            if (chart == null) return;
+
+            chart.Series.Clear();
+            var series = new Series
+            {
+                Name = "Empréstimos",
+                ChartType = SeriesChartType.Column,
+                IsVisibleInLegend = false
+            };
+
+            int efetuados = emprestimos.Count;
+            int andamento = emprestimos.Count(e => e.Status == "Em andamento");
+            int atraso = emprestimos.Count(e => e.Status == "Em atraso");
+
+            series.Points.AddXY("Efetuados", efetuados);
+            series.Points.AddXY("Em Andamento", andamento);
+            series.Points.AddXY("Em Atraso", atraso);
+
+            chart.Series.Add(series);
+        }
+
+        private List<Emprestimo> ObterTodosEmprestimos()
+        {
+            List<Emprestimo> lista = new List<Emprestimo>();
+            string query = @"SELECT e.id, u.nome AS NomeUsuario, l.titulo AS TituloLivro,
+                            e.data_emprestimo, e.data_devolucao, e.data_devolvido, e.status
+                     FROM emprestimos e
+                     INNER JOIN usuarios u ON e.id_usuario = u.id
+                     INNER JOIN livros l ON e.id_livro = l.id";
+
+            using (MySqlCommand cmd = new MySqlCommand(query, conexao))
+            {
+                conexao.Open();
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var emp = new Emprestimo
+                        {
+                            Id = reader.GetInt32("id"),
+                            NomeUsuario = reader.GetString("NomeUsuario"),
+                            TituloLivro = reader.GetString("TituloLivro"),
+                            DataEmprestimo = reader.GetDateTime("data_emprestimo"),
+                            DataDevolucao = reader.IsDBNull(reader.GetOrdinal("data_devolucao"))
+                                ? (DateTime?)null : reader.GetDateTime("data_devolucao"),
+                            DataDevolvido = reader.IsDBNull(reader.GetOrdinal("data_devolvido"))
+                                ? (DateTime?)null : reader.GetDateTime("data_devolvido"),
+                            Status = reader.GetString("status"),
+                        };
+                        lista.Add(emp);
+                    }
+                }
+                conexao.Close();
+            }
+            return lista;
         }
     }
 }
