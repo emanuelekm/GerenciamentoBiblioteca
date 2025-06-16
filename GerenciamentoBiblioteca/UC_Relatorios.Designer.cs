59a43f351d5c600ffe1fd3c763f8351d47e231a5
emanuelekm <emanuelekmiecik1507@gmail.com>
2025-06-16 10:47:08 -0300

    atualizações

--- GerenciamentoBiblioteca/UC_Relatorios.Designer.cs
+++ GerenciamentoBiblioteca/UC_Relatorios.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.chartEmprestimo = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridViewEmprestimo = new System.Windows.Forms.DataGridView();
@@ -46,24 +46,24 @@
             this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
             this.label1.Location = new System.Drawing.Point(41, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 28);
+            this.label1.Size = new System.Drawing.Size(471, 28);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Relatórios";
+            this.label1.Text = "Relatório de Empréstimos e Devoluções";
             // 
             // chartEmprestimo
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chartEmprestimo.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartEmprestimo.Legends.Add(legend2);
-            this.chartEmprestimo.Location = new System.Drawing.Point(46, 106);
+            chartArea1.Name = "ChartArea1";
+            this.chartEmprestimo.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartEmprestimo.Legends.Add(legend1);
+            this.chartEmprestimo.Location = new System.Drawing.Point(46, 445);
             this.chartEmprestimo.Name = "chartEmprestimo";
             this.chartEmprestimo.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartEmprestimo.Series.Add(series2);
-            this.chartEmprestimo.Size = new System.Drawing.Size(597, 344);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartEmprestimo.Series.Add(series1);
+            this.chartEmprestimo.Size = new System.Drawing.Size(597, 383);
             this.chartEmprestimo.TabIndex = 1;
             this.chartEmprestimo.Text = "chartEmprestimo";
             // 
@@ -74,22 +74,23 @@
             this.dataGridViewEmprestimo.BackgroundColor = System.Drawing.SystemColors.ControlLight;
             this.dataGridViewEmprestimo.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridViewEmprestimo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewEmprestimo.Location = new System.Drawing.Point(46, 532);
+            this.dataGridViewEmprestimo.Location = new System.Drawing.Point(46, 112);
             this.dataGridViewEmprestimo.Name = "dataGridViewEmprestimo";
             this.dataGridViewEmprestimo.ReadOnly = true;
             this.dataGridViewEmprestimo.RowHeadersWidth = 62;
             this.dataGridViewEmprestimo.RowTemplate.Height = 28;
-            this.dataGridViewEmprestimo.Size = new System.Drawing.Size(1291, 310);
+            this.dataGridViewEmprestimo.Size = new System.Drawing.Size(1333, 310);
             this.dataGridViewEmprestimo.TabIndex = 2;
             this.dataGridViewEmprestimo.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dataGridViewEmprestimo_RowPrePaint);
             // 
             // buttonExportar
             // 
-            this.buttonExportar.Location = new System.Drawing.Point(1108, 431);
+            this.buttonExportar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExportar.Location = new System.Drawing.Point(1097, 32);
             this.buttonExportar.Name = "buttonExportar";
-            this.buttonExportar.Size = new System.Drawing.Size(217, 69);
+            this.buttonExportar.Size = new System.Drawing.Size(282, 56);
             this.buttonExportar.TabIndex = 3;
-            this.buttonExportar.Text = "Exportar dados paraPDF";
+            this.buttonExportar.Text = "Exportar dados em PDF";
             this.buttonExportar.UseVisualStyleBackColor = true;
             this.buttonExportar.Click += new System.EventHandler(this.buttonExportar_Click);
             // 
@@ -102,7 +103,7 @@
             this.Controls.Add(this.chartEmprestimo);
             this.Controls.Add(this.label1);
             this.Name = "UC_Relatorios";
-            this.Size = new System.Drawing.Size(1378, 874);
+            this.Size = new System.Drawing.Size(1505, 939);
             ((System.ComponentModel.ISupportInitialize)(this.chartEmprestimo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmprestimo)).EndInit();
             this.ResumeLayout(false);
