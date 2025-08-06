4fa334e4624299cf70b0a5f6acc8f743f711143b
emanuelekm <emanuelekmiecik1507@gmail.com>
2025-08-05 21:26:15 -0300

    // Cores do relatório

--- GerenciamentoBiblioteca/UC_Relatorios.cs
+++ GerenciamentoBiblioteca/UC_Relatorios.cs
@@ -68,10 +68,17 @@ namespace GerenciamentoBiblioteca
             series.Points.AddXY("Devolvido c/ atraso", devolvidoAtraso);
             series.Points.AddXY("Devolvido", devolvido);
 
-            series.Points[0].Color = Color.Yellow;
-            series.Points[1].Color = Color.Orange; 
-            series.Points[2].Color = Color.Red;
-            series.Points[3].Color = Color.LightGreen;
+            //series.Points[0].Color = Color.Yellow;
+            series.Points[0].Color = ColorTranslator.FromHtml("#f5f522");
+
+            //series.Points[1].Color = Color.Orange;
+            series.Points[1].Color = ColorTranslator.FromHtml("#f2b21b");
+
+            //series.Points[2].Color = Color.Red;
+            series.Points[2].Color = ColorTranslator.FromHtml("#d44041");
+
+            //series.Points[3].Color = Color.LightGreen;
+            series.Points[3].Color = ColorTranslator.FromHtml("#0ecf15");
 
             chartEmprestimo.Legends[0].Docking = Docking.Bottom;
             chart.Series.Add(series);
@@ -269,8 +276,10 @@ namespace GerenciamentoBiblioteca
             serie.Points.AddXY("Disponíveis", disponiveis);
             serie.Points.AddXY("Indisponíveis", indisponiveis);
 
-            serie.Points[0].Color = Color.LightGreen;
-            serie.Points[1].Color = Color.Red;
+            //serie.Points[0].Color = Color.LightGreen;
+            serie.Points[0].Color = ColorTranslator.FromHtml("#0ecf15");
+            //serie.Points[1].Color = Color.Red;
+            serie.Points[1].Color = ColorTranslator.FromHtml("#d44041");
 
             chartLivroDisponibilidade.Legends[0].Docking = Docking.Bottom;
 
@@ -332,14 +341,19 @@ namespace GerenciamentoBiblioteca
                 ChartType = SeriesChartType.Bar,
                 IsVisibleInLegend = false
             };
-
+            
             series.Points.AddXY("Bom", dados["Bom"]);
             series.Points.AddXY("Regular", dados["Regular"]);
             series.Points.AddXY("Ruim", dados["Ruim"]);
 
-            series.Points[0].Color = Color.LightGreen;
-            series.Points[1].Color = Color.Yellow;
-            series.Points[2].Color = Color.Red;
+            //series.Points[0].Color = Color.LightGreen;
+            series.Points[0].Color = ColorTranslator.FromHtml("#0ecf15");
+            
+            //series.Points[1].Color = Color.Yellow;
+            series.Points[1].Color = ColorTranslator.FromHtml("#f5f522");
+
+            //series.Points[2].Color = Color.Red;
+            series.Points[2].Color = ColorTranslator.FromHtml("#d44041");
 
             chart.Series.Add(series);
 
